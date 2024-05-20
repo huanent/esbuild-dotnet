76a11e735d6514aefa2c41a89b6eef0b1f9d285e
huanent <huanent@icloud.com>
2024-05-21 00:07:40 +0800

    Add rid mapping

--- Esbuild/Bundler.cs
+++ Esbuild/Bundler.cs
@@ -7,9 +7,20 @@ public static class Bundler
 {
     private static readonly string bin;
     private static readonly string binFileName;
-    private static readonly string rid = RuntimeInformation.RuntimeIdentifier;
+    private static readonly Dictionary<string, string> ridMapping = new()
+    {
+        {"win-x64", "win-x64"},
+        {"linux-x64", "linux-x64"},
+        {"linux-musl-x64", "linux-x64"},
+        {"linux-arm64", "linux-arm64"},
+        {"linux-musl-arm64", "linux-arm64"},
+        {"osx-x64", "osx-x64"},
+        {"osx-arm64", "osx-arm64"},
+    };
+
     static Bundler()
     {
+        var rid = ridMapping[RuntimeInformation.RuntimeIdentifier];
         binFileName = rid.StartsWith("win-") ? "esbuild.exe" : "esbuild";
         bin = Path.Combine(AppContext.BaseDirectory, binFileName);
         if (File.Exists(bin)) return;
